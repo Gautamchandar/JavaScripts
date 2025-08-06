bccba6aa85ed34851a66c5fffeb6d611498c47fb
Gautam Naugai <gautamnogai@gmail.com>
2025-08-06 16:06:21 +0530

    jspart1file

---
+++ JSPart1.js
@@ -0,0 +1,12 @@
+//Temporal Dead Zone
+
+console.log(a);
+let a = 6; // Uncaught ReferenceError: Cannot access 'a' before initialization
+
+//Data Types
+
+// primitive = String, null, undefined, boolean, number, bigint, symbol
+
+// reference = Object{}, Array[], Function()
+
+ 
