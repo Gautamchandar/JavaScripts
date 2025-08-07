631909fdb2a104d9508a55f7e5aac10683528150
Gautam Naugai <gautamnogai@gmail.com>
2025-08-07 09:41:19 +0530

    part2

---
+++ JsPart2.js
@@ -0,0 +1,15 @@
+//Data Types
+
+// primitive = String, null, undefined, boolean, number, bigint, symbol
+
+// reference = Object{}, Array[], Function()
+
+12 = Number
+12.5 = Number
+
+True = Boolean
+false = Boolean
+
+let bool = null
+let valueAssign = undefined
+
