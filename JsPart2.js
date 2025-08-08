f4d4a79095dfb32d4bb406c97e1e5996ac339810
Gautam Naugai <gautamnogai@gmail.com>
2025-08-08 10:03:42 +0530

    part2

--- JsPart2.js
+++ JsPart2.js
@@ -4,12 +4,26 @@
 
 // reference = Object{}, Array[], Function()
 
-12 = Number
-12.5 = Number
+// 12 = Number
+// 12.5 = Number
 
-True = Boolean
-false = Boolean
+// True = Boolean
+// false = Boolean
 
-let bool = null
-let valueAssign = undefined
+// let bool = null
+// let valueAssign = undefined
 
+let a = 7373729372373783n
+console.log(a); // This is Bigint
+
+console.log(typeof true);
+
+console.log("5" + 1)
+console.log("5" - 1)
+
+// Truthy and Falsy 
+
+console.log(true+false)
+console.log(null+1)
+console.log(5+"5")
+console.log(!!undefined)
