824c96a9b31857e734590e104f90673badb12409
Gautam Naugai <gautamnogai@gmail.com>
2025-08-16 22:26:32 +0530

    commited

---
+++ jspart3.js
@@ -0,0 +1,25 @@
+//Operators
+
+console.log("Operators")
+
+console.log(12==12)
+console.log(12===12)
+console.log(12=="12")
+console.log(12==="12")
+console.log(12!="12")
+console.log(12!=="12")
+
+//Arthematic Operators
+console.log(1+2)
+console.log(1-2)
+console.log(1*2)
+console.log(1/2)
+console.log(1%2)
+console.log(1**2)
+
+//Logical Operators
+
+console.log(1>2 && 2<10)
+console.log(1>2 || 2<10)
+console.log(!(1>2))
+
