25d340b1772954ef557a7ae7c794dfec93f35596
Gautam Naugai <gautamnogai@gmail.com>
2025-08-17 17:31:08 +0530

    questionsolved

--- conditionals.js
+++ conditionals.js
@@ -56,3 +56,32 @@ let login = true;
 //     default:
 //         console.log("Invaild Day")
 // }
+
+
+let score = 88;
+
+function getGrade(){
+    if(score < 100 && score >= 90){
+        console.log("A+")
+    }
+    else if(score >= 80 && score < 90){
+        console.log("A")
+    }
+    else if(score >= 70 && score < 80){
+        console.log("B")
+    }
+    else if(score >= 60 && score < 70){
+        console.log("C")
+    }
+    else if(score >= 33 && score < 60){
+        console.log("D")
+    }
+    else if( score >= 0 && score < 33){
+        console.log("Fail")
+    }
+    else{
+        console.log("Invalid Marks ❌")
+    }
+}
+
+getGrade(score)
