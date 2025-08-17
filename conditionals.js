595e17e0cd983d78927f2771995885ac572361db
Gautam Naugai <gautamnogai@gmail.com>
2025-08-17 17:07:58 +0530

    commited

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+// let a = 78;
+// let b = 100;
+
+// if(a < b){
+//     console.log("Greater");
+// }
+// else{
+//     console.log("Not Greater")
+// }
+
+let login = true;
+
+if(login){
+    console.log("Access Granted")
+}
+else{
+    console.log("Denied")
+}
