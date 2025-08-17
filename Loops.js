872c3f35f5e0678d59b230103af2823eb7cdc62b
Gautam Naugai <gautamnogai@gmail.com>
2025-08-17 20:30:39 +0530

    whiledowhile

--- Loops.js
+++ Loops.js
@@ -6,6 +6,23 @@
 
 // for loop 
 
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// while loop
+
+// let peak = 1;
+// while(peak < "5"){
+//     console.log(peak)
+//     peak++
+// }
+
+//do-while
+
+let i = 21
+do {
+    console.log(i)
+    i++
+} while (i < 20 ); //it aleast one time print value if condition is false
+
