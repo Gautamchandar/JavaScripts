ea09c375af4036becb2b612b5aa337e86705f7b4
Gautam Naugai <gautamnogai@gmail.com>
2025-08-17 20:14:41 +0530

    forLoopCommited

---
+++ Loops.js
@@ -0,0 +1,11 @@
+// Loops means repeation 
+
+// for while and do while for in for each 
+
+// jab hame future ke target nhi pta nhi chal jata wo target kya jhe tho while loop use hoga aur jab hame pta ho target kya he tb for loop use hoga 
+
+// for loop 
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
