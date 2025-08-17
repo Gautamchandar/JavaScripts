5149745d5389db9cd3dd3e5cbc4f777587a0eebd
Gautam Naugai <gautamnogai@gmail.com>
2025-08-17 17:03:12 +0530

    commited

--- jspart3.js
+++ jspart3.js
@@ -1,25 +1,54 @@
-//Operators
+// //Operators
 
-console.log("Operators")
+// console.log("Operators")
 
-console.log(12==12)
-console.log(12===12)
-console.log(12=="12")
-console.log(12==="12")
-console.log(12!="12")
-console.log(12!=="12")
+// console.log(12==12)
+// console.log(12===12)
+// console.log(12=="12")
+// console.log(12==="12")
+// console.log(12!="12")
+// console.log(12!=="12")
 
-//Arthematic Operators
-console.log(1+2)
-console.log(1-2)
-console.log(1*2)
-console.log(1/2)
-console.log(1%2)
-console.log(1**2)
+// //Arthematic Operators
+// console.log(1+2)
+// console.log(1-2)
+// console.log(1*2)
+// console.log(1/2)
+// console.log(1%2)
+// console.log(1**2)
 
-//Logical Operators
+// //Logical Operators
 
-console.log(1>2 && 2<10)
-console.log(1>2 || 2<10)
-console.log(!(1>2))
+// console.log(1>2 && 2<10)
+// console.log(1>2 || 2<10)
+// console.log(!(1>2))
 
+//Unary Operator
+
+// + , - , ++ , -- 
+
+//ternary Operator
+
+// ?: 
+// 1<2? console.log("true")
+// :console.log("false");
+
+
+// type of and instance of
+
+// typeof null is object becoz galat btata h 
+
+// type of array is object 
+
+// let a = []
+// console.log(a instanceof Array); //True
+
+// let b = {}
+// console.log(a instanceof Object); //False
+
+// instanceof only works on refrence value not primitive value
+
+// let a = Boolean(0);
+// console.log(a);
+
+ 
