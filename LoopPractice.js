b5f66590092a0f10cfeaf382bc0f8a5f81450492
Gautam Naugai <gautamnogai@gmail.com>
2025-08-18 23:29:01 +0530

    breakAndContinue

--- LoopPractice.js
+++ LoopPractice.js
@@ -57,20 +57,53 @@
 
 // 8. Ask the user for a number and print whether each number from 1 to that number is even or odd
 
-let user = prompt("Enter any number")
+// let user = prompt("Enter any number")
 
-for (let i = 1; i <= user; i++) {
-        if(i % 2 === 0){
-            console.log(`${i} even`)
-        } else{
-            console.log(`${i} odd`)
-        }
-    }
+// for (let i = 1; i <= user; i++) {
+//         if(i % 2 === 0){
+//             console.log(`${i} even`)
+//         } else{
+//             console.log(`${i} odd`)
+//         }
+//     }
 
 // 9. Count how many numbers between 1 to 100 are divisible by both 3 and 5
 
-for (let i = 0; i < 100; i++) {
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log(i)
-    } 
+// for (let i = 0; i < 100; i++) {
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log(i)
+//     } 
+// }
+
+// 10. Break and Continue Questions 
+
+// Q1: Stop at First Multiple of 7
+
+// Write a loop from 1 to 100 that:
+//   • Prints each number
+//   • Stops completely when it finds the first number divisible by 7
+
+for (let i = 1; i < 101; i++) {
+    if(i % 7 === 0){
+        break
+    }
+    console.log(i)
+}
+
+// Q2: Skip-Multiples-of-3
+
+// Write a loop from 1 to 20 that:
+//   • Skips numbers divisible by 3
+//   • Prints all others
+
+// 💡 Use continue
+
+// Expected output:
+// 1 2 4 5 7 8 10 11 ... (no 3, 6, 9, etc.)
+
+for (let i = 0; i < 21; i++) {
+    if(i % 3 == 0){
+        continue
+    }
+    console.log(i)
 }
