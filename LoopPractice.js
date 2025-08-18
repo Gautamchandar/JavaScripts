3fde63856501b27553024e32da8faafd19f82a45
Gautam Naugai <gautamnogai@gmail.com>
2025-08-18 22:43:30 +0530

    4QuestionCompleted

---
+++ LoopPractice.js
@@ -0,0 +1,34 @@
+// Print number from 1 to 10 using for loop 
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+    
+}
+
+// Print number from 10 to 1 using while loop 
+
+let j = 10;
+
+while(j > 0){
+    console.log(j)
+    j--
+}
+
+// Print even number from 1 to 20 using for loop 
+
+for (let i = 1; i < 21; i++) {
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
+
+// Print odd number from 1 to 15 using while loop 
+
+let i = 1;
+while(i < 16){
+    if(i % 2 == 1){
+        console.log(i)
+    }
+    i++
+}
+
